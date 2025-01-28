50e6d0235df26a52b1635437a41d99dc50118f89
Zain-Abbas <zain786@outlook.com>
2025-01-28 15:10:05 -0800

    updated program to find all connected components by finding root of each Node in the graph (using find() to calculate root)

--- src/CliqueDetector.java
+++ src/CliqueDetector.java
@@ -1,13 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+
 // store N, M
 public class CliqueDetector {
     static int N, M;
-    static int[][] adjMatrix; // create 2D Matrix to store edges
     static int[] degree; //
     static List<Set<Integer>> adjList; // to store
     // ex) 1: {
@@ -50,8 +51,7 @@ public class CliqueDetector {
                 parent[rootP] = rootQ;
                 // 2) update q size by adding subtree p
                 size[rootQ] += size[rootP];
-            }
-            else {
+            } else {
                 // otherwise p >= q
                 // if p == q: rootP is the parent of rootQ (for consistency)
                 // else(size[rootP] >= size[rootQ])
@@ -84,9 +84,9 @@ public class CliqueDetector {
             // token[0] -> N
             // token[1] -> M
             N = Integer.parseInt(tokens[0]);
-           // System.out.println("reading N: " + N);
+            // System.out.println("reading N: " + N);
             M = Integer.parseInt(tokens[1]);
-          //  System.out.println("reading M: " + M);
+            //  System.out.println("reading M: " + M);
 
             // create adjacency list
             // 1 2
@@ -107,7 +107,7 @@ public class CliqueDetector {
             adjList = new ArrayList<>();
             //iterate over every Node in the graph
             // creating list of sets (each
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i <= N; i++) {
                 adjList.add(new HashSet<>());
                 // HashSet<> : represents
             }
@@ -120,9 +120,9 @@ public class CliqueDetector {
                 // tokens -> reading each line from file (not including first)
                 tokens = br.readLine().split("\\s+");
                 // retrieve (x,y) each edge in the graph
-                int x = Integer.parseInt(tokens[0]) - 1;
-               // System.out.println("x: " + x);
-                int y = Integer.parseInt(tokens[1]) - 1;
+                int x = Integer.parseInt(tokens[0]);
+                // System.out.println("x: " + x);
+                int y = Integer.parseInt(tokens[1]);
                 //System.out.println("y: " + y);
                 // your code here
                 // adjList.get(x): retrieves set associated with node x (contains all neighbors of x)
@@ -133,47 +133,86 @@ public class CliqueDetector {
                 adjList.get(x).add(y); //
                 adjList.get(y).add(x); // for symmetry (undirected 1,2 = 2,1)
                 // Print the adjacency list after adding the edge (x, y)
-                System.out.println("Adjacency List after adding edge (" + (x + 1) + ", " + (y + 1) + "):");
-                for (int j = 0; j < N; j++) {
-                    System.out.print((j + 1) + ": " + adjList.get(j) + " ");
+                System.out.println("Adjacency List after adding edge (" + x + ", " + y + "):");
+                for (int j = 1; j <= N; j++) {
+                    System.out.print(j + ": " + adjList.get(j) + " ");
                 }
                 System.out.println();  // Print a newline after each printout
 
             }
-          //  System.out.println(" " + adjList.get(0));
-
 
 
             br.close();
             // get degree (calculated by finding number of connections of each node)
-            degree = new int[N]; // 1) initialize array
-            for (int i = 0; i < N; i++) { // find #of connections of each Node
+            degree = new int[N+1]; // 1) initialize array
+            for (int i = 1; i <= N; i++) { // find #of connections of each Node
                 degree[i] = adjList.get(i).size(); // store number of connections in array
                 //System.out.println("for node "+ (i + 1) + " " + degree[i] + " ");
-               // System.out.println("neighbors of node "+ (i + 1) + " " + adjList.get(i) + " ");
+                // System.out.println("neighbors of node "+ (i + 1) + " " + adjList.get(i) + " ");
             }
             // example: adjList.get(0).size() -> getting number of connected components of node 1 (i + 1)
             // adjList.get(i) -> neighbors in node i
             //
 
             //1) call Union-Find algorithm to find connected components
-            UnionFind uf = new UnionFind(N); //
-            for (int i = 0; i < N; i++) { // go through each node in graph
-                for (int neighbor: adjList.get(i)) { // go through all neighbors of node i
-                    uf.union(neighbor, i); //
+            UnionFind uf = new UnionFind(N + 1); //
+            for (int i = 1; i <= N; i++) { // go through each node in graph
+                for (int neighbor : adjList.get(i)) { // go through all neighbors of node i
+                    uf.union(neighbor, i); // construct the graph
+                    // neighbor -> connections of Node i
+                    // i -> Node
 
                 }
             }
 
+            // 2) Find all connected Components
+            // Map<Integer, Integer> map = new HashMap<>(); // create Components map
+            var map = new int[N + 1];
+            // key = root of connected component
+            // value = list of nodes in component
+            for (int i = 1; i <= N; i++) {
+                int root = uf.find(i); // find root of each Node
+                map[i] = root;
+                System.out.println(i + " " + root);
+            }
+            // cc stores distinct connected components
+            var cc = new ArrayList<Integer>();
+            Arrays.sort(map); // put all the equal items together
+            cc.add(map[1]);
+            for (int x = map[1], i = 2; i <= N; i++) {
+                if (x != map[i]) {
+                    x = map[i];
+                    cc.add(x);
+                }
+            }
             int maxScore = 0;
+            for (int i = 0; i < cc.size(); i++) {
+                int c = cc.get(i);
+                var ns = new ArrayList<Integer>();
+                for (int n = 1; n <= N; n++) {
+                    if (map[n] == c) ns.add(n);
+                }
+                if (ns.size() == 1) {
+                    continue;
+                }
+                if (ns.size() == 2) {
+                    maxScore = Math.max(maxScore, 2);
+                    continue;
+                }
+                //
+
+
+
+            }
+
             // your code here
 
             // Output the result
-           // System.out.println(maxScore);
+            // System.out.println(maxScore);
         } catch (IOException e) {
             System.err.println("An error occurred while reading the input file:");
             e.printStackTrace();
             System.exit(1);
         }
     }
-    }
+}
